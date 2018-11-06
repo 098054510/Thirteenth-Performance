5ae3ba30b86b59a849a5fd233e1485250e525f47
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:28:40 -0300

    Thirdteenth Performance/Program made in C#

---
+++ ThirteenthPerformance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThirteenthPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int volume, comando;
+            volume = int.Parse(Console.ReadLine());
+            comando = int.Parse(Console.ReadLine());
+
+            while(comando != 0)
+            {
+                volume = volume + comando;
+                if(volume < 0)
+                {
+                    volume = 0;
+                }
+                if (volume > 100)
+                {
+                    volume = 100;
+                }
+                Console.WriteLine(volume);
+                comando = int.Parse(Console.ReadLine());
+            }
+            Console.ReadLine();
+        }
+    }
+}
